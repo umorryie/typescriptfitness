f1b3a6d147edd09adda46bc5f16ea46bc06a5a7b
Matej <pesjak.matej@gmail.com>
2021-04-16 23:05:42 +0200

    reverted changes for DB

--- server/config.ts
+++ server/config.ts
@@ -1,6 +1,6 @@
 import { config as configDotenv } from 'dotenv';
 configDotenv();
-const configFREEOLD = {
+const config = {
     user: process.env.DB_USER || "sql11401137",
     database: process.env.DB_DATABASE || 'sql11401137',
     password: process.env.DB_PASSWORD || 'kSdbm23w9n',
@@ -8,15 +8,7 @@ const configFREEOLD = {
     port: process.env.PORT || 3306,
     secret: process.env.JWT_SECRET || "3pKrm1220zvvHzPaowZ96a0OOZdbzXObI9FDMgWYqgjo"
 }
-const configHEROKU = {
-    user: process.env.DB_USER || "bf44d108e59568",
-    database: process.env.DB_DATABASE || 'heroku_ecbf54b0074dafd',
-    password: process.env.DB_PASSWORD || '8c6d3d6a',
-    host: process.env.DB_HOST || 'eu-cdbr-west-01.cleardb.com',
-    port: process.env.PORT || 3306,
-    secret: process.env.JWT_SECRET || "3pKrm1220zvvHzPaowZ96a0OOZdbzXObI9FDMgWYqgjo"
-}
 
 module.exports = {
-    config: configFREEOLD
+    config
 }
